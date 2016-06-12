b13771dc0d3840d86d1b23aebe7cd777768da8e1
morrillo <gustavo.orrillo@moldeo.coop>
2016-06-12 12:06:36 -0300

    Added model

--- models.py
+++ models.py
@@ -8,4 +8,16 @@ import logging
 import datetime
 from openerp.fields import Date as newdate
 
+#Get the logger
+_logger = logging.getLogger(__name__)
 
+
+class partnerinfo_comparison(models.Model):
+	_name = 'partnerinfo.comparison'
+
+	product_tmpl_id = fields.Many2one('product.template',string='Product')
+	pricelist_id = fields.Many2one('product.pricelist',string='Pricelist')
+	supplier_id = fields.Many2one('res.partner',string='Supplier')
+	currency_id = fields.Many2one('res.currency',related='pricelist_id.currency_id')
+	qty = fields.Integer(string='Qty')
+	price = fields.Float(string='Price')
